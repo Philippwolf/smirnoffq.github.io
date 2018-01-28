7250f658a61c7f524038072f0f922efed13a3a74
Smirnoffq <bartlomiej.gorkiewicz@gmail.com>
2018-01-28 21:01:55 +0100

    v9.1.3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var isPaused = 0;
 
 var text = 
-'{"0":{"text":"Hello there! My name is Bartek, and this is my portfolio website :)","wait":2},"1":{"text":"Looks empty? Huh... maybe that\'s because I\'m 21 years old, and I don\'t have a lot of real world project on account.","wait":3},"2":{"text":"Maybe before I show you what I have I will tell you something about myself.","wait":2},"3":{"text":"In 2017 I started learning Computer Science on University of Technology in City of Łódź in Poland. Until then I self-taught myself web technologies (prefer backend (PHP)), and a little bit of programming in Python and C.","wait":2},"4":{"text":"Everything you can see here is what I self-taught myself.","wait":2},"5":{"text":"Okay.. thats enough about me, of course I put my CV somewhere down below, so if you want to know something more about me, go ahead and check it out, and if you want to contact me directly here is my email and skype: ","wait":2},"6":{"text":"Mail: bartlomiej.gorkiewicz@gmail.com","wait":1},"7":{"text":"Skype: Smirnoffq","wait":2},"8":{"text":"Now here are my projects (not all, but \'the best\' of what I can show in public ;) )","wait":3},"9":{"text":"I have to go now. If anything here was interesting for you feel free to contact me!","wait":1},"10":{"text":"...also my CV I mentioned earlier: Bartłomiej Górkiewicz CV.","wait":2},"11":{"text":"My very advanced AI will tell you more about every project in right panel, just click on what you are interested in.","wait":2},"12":{"text":"Bye for now...","wait":0}}'
+'{"0":{"text":"Hello there! My name is Bartek, and this is my portfolio website :)","wait":2},"1":{"text":"Looks empty? Huh... maybe that\'s because I\'m 21 years old, and I don\'t have a lot of real world projects on account.","wait":3},"2":{"text":"Maybe before I show you what I have I will tell you something about myself.","wait":2},"3":{"text":"In 2017 I started learning Computer Science on University of Technology in City of Łódź in Poland. Until then I taught myself web technologies (prefer backend (PHP)), and a little bit of programming in Python and C.","wait":2},"4":{"text":"Okay.. thats enough about me, of course I put my CV somewhere down below, so if you want to know something more about me, go ahead and check it out, and if you want to contact me directly here is my email and skype: ","wait":2},"5":{"text":"Mail: bartlomiej.gorkiewicz@gmail.com","wait":1},"6":{"text":"Skype: Smirnoffq","wait":2},"7":{"text":"Now here are my projects (not all, but \'the best\' of what I can show in public ;) )","wait":3},"8":{"text":"I have to go now. If anything here was interesting for you feel free to contact me!","wait":1},"9":{"text":"...also my CV I mentioned earlier: Bartłomiej Górkiewicz CV.","wait":2},"10":{"text":"My very advanced AI will tell you more about every project in right panel, just click on what you are interested in.","wait":2},"11":{"text":"Bye for now...","wait":0}}'
 ;
 
 var dets = 
@@ -17,7 +17,7 @@ function writeText(text, index, i, mode = "t", indexD = 0) {
 	if (mode == "t")
 	{
 		// pojawienie sie projektow
-		if (index == 8) 
+		if (index == 7) 
 		{
 			if ($(window).width() > 1225) 
 			{
@@ -40,10 +40,11 @@ function writeText(text, index, i, mode = "t", indexD = 0) {
 		{
 			if (i < text[index].text.length) {
 				document.getElementById("tekst-div-pre").innerHTML += text[index].text[i];
+				document.getElementById("tekst-div").scrollTop = document.getElementById("tekst-div").scrollHeight;
 				i++;
 				setTimeout(function() {writeText(text, index, i, mode)}, oneLetterTime);
 			} else {
-				if (index == 7) document.getElementById("tekst-div-pre").innerHTML += "<br />";
+				if (index == 6) document.getElementById("tekst-div-pre").innerHTML += "<br />";
 				document.getElementById("tekst-div-pre").innerHTML += "<br /><br />";
 				timeout = (text[index].wait) * 1000
 				index++;
@@ -62,7 +63,8 @@ function writeText(text, index, i, mode = "t", indexD = 0) {
 		if (indexD <= Object.keys(text).length)
 		{
 			if (i < text[index][indexD].length) {
-				document.getElementById("tekst-div-pre").innerHTML += text[index][indexD][i]
+				document.getElementById("tekst-div-pre").innerHTML += text[index][indexD][i];
+				document.getElementById("tekst-div").scrollTop = document.getElementById("tekst-div").scrollHeight;
 				i++;
 				setTimeout(function() {writeText(text, index, i, mode, indexD)}, oneLetterTime);
 
